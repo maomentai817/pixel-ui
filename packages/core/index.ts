3d7c586fb2a2c18f114ac7bf45a63f96ac3728a3
mmt817 <1669404371@qq.com>
2025-03-22 22:19:31 +0800

    chore: @hackernoon/pixel-icon图标库引入

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,5 +1,6 @@
 import { makeInstaller } from "@pixel-ui/utils"
 import components from "./components"
+import "@hackernoon/pixel-icon-library/fonts/iconfont.css"
 import '@pixel-ui/theme/index.css'
 
 const installer = makeInstaller(components)
--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import PixelUI from 'pixel-ui'
+import "@hackernoon/pixel-icon-library/fonts/iconfont.css"
 import 'virtual:uno.css'
 
 const app = createApp(App)
