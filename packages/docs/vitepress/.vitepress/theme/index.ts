3a6a817333e25aa6aa993425206dafe0cc26b3e2
mmt817 <1669404371@qq.com>
2025-04-22 16:23:35 +0800

    chore: PS2P/Zpix字体baseline修正 feat: vitepress首屏rainbow动效优化

--- packages/docs/vitepress/.vitepress/theme/index.ts
+++ packages/docs/vitepress/.vitepress/theme/index.ts
@@ -1,4 +1,5 @@
-import { type App } from "vue"
+import { type App, watch } from "vue"
+import { type Router } from "vitepress"
 import { ElementPlusContainer } from "vitepress-preview-component"
 import DefaultTheme from "vitepress/theme"
 import PixelUI from "@mmt817/pixel-ui"
@@ -9,10 +10,43 @@ import '@mmt817/pixel-ui/dist/index.css'
 import 'virtual:uno.css'
 import './index.css'
 
+let homePageStyle: HTMLStyleElement | undefined
+
 export default {
   ...DefaultTheme,
-  enhanceApp({ app }: { app: App }) {
+  enhanceApp({ app, router }: { app: App, router: Router }) {
     app.component("demo-preview", ElementPlusContainer)
     app.use(PixelUI)
+
+    // 彩虹背景动画样式
+    if (typeof window !== 'undefined') {
+      watch(
+        () => router.route.data.relativePath,
+        () => updateHomePageStyle(location.pathname === '/pixel-ui/'),
+        { immediate: true },
+      )
+    }
   },
+}
+
+// 彩虹背景动画样式控制
+function updateHomePageStyle(value: boolean) {
+  if (value) {
+    if (homePageStyle)
+      return
+
+    homePageStyle = document.createElement('style')
+    homePageStyle.innerHTML = `
+    :root {
+      animation: rainbow 12s linear infinite;
+    }`
+    document.body.appendChild(homePageStyle)
+  }
+  else {
+    if (!homePageStyle)
+      return
+
+    homePageStyle.remove()
+    homePageStyle = undefined
+  }
 }
