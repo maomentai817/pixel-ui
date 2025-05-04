7b06627fffaad3207bc875090746380d59561900
mmt817 <1669404371@qq.com>
2025-05-04 13:19:45 +0800

    feat: vp-api-enum插件实现/挂载

--- packages/docs/vitepress/.vitepress/vitepress/index.ts
+++ packages/docs/vitepress/.vitepress/vitepress/index.ts
@@ -1,9 +1,11 @@
 import ApiTyping from './components/globals/vp-api-typing.vue'
+import ApiEnum from './components/globals/vp-api-enum.vue'
 import bsz from './components/bsz.vue'
 import type { Component } from 'vue'
 
 export const globals: [string, Component][] = [
   ['ApiTyping', ApiTyping],
+  ['ApiEnum', ApiEnum],
 ]
 
 export default bsz
