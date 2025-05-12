0a39b6463d609d2353bce8c2f0852f9029640e0a
mmt817 <1669404371@qq.com>
2025-05-12 21:44:03 +0800

    feat: 组件库国际化相关

---
+++ packages/locale/index.ts
@@ -0,0 +1,12 @@
+export { default as en } from './lang/en'
+export { default as zhCN } from './lang/zh-cn'
+export { default as zhTW } from './lang/zh-tw'
+
+export type TranslatePair = {
+  [key: string]: string | string[] | TranslatePair
+}
+
+export type Language = {
+  name: string
+  el: TranslatePair
+}
