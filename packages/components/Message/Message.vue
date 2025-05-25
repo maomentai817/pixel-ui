5db59caf0e1860fcc294e766211dcabb0d00033e
mmt817 <1669404371@qq.com>
2025-05-25 15:24:30 +0800

    feat: PxMessage结构搭建

--- packages/components/Message/Message.vue
+++ packages/components/Message/Message.vue
@@ -1,11 +1,93 @@
 <script setup lang="ts">
+import { computed, onMounted, ref, watch } from 'vue'
+import { delay, bind } from 'lodash-es'
+import { addUnit, typeIconMap } from '@pixel-ui/utils'
+
+import type { MessagePropsIn, MessageCompInstance } from './types'
+
+import PxIcon from '../Icon/Icon.vue'
+
 const COMP_NAME = 'PxMessage' as const
 defineOptions({
   name: COMP_NAME
 })
+
+const props = withDefaults(defineProps<MessagePropsIn>(), {
+  type: 'info',
+  duration: 3000,
+  offset: 10,
+  transitionName: 'fade-up'
+})
+
+const visible = ref(false)
+const messageRef = ref<HTMLDivElement>()
+const iconName = computed(
+  () => typeIconMap.get(props.type) ?? 'info-circle-solid'
+)
+// div 高度
+const boxHeight = ref(0)
+
+// ⏲ message 计时
+let timer: number
+const startTimer = () => {
+  if (props.duration === 0) return
+  timer = delay(close, props.duration)
+}
+const clearTimer = () => {
+  clearTimeout(timer)
+}
+const close = () => {
+  visible.value = false
+}
+
+// 退出动画
+watch(visible, (val) => {
+  if (!val) boxHeight.value = -props.offset
+})
+
+onMounted(() => {
+  visible.value = true
+  startTimer()
+})
+
+defineExpose<MessageCompInstance>({
+  bottomOffset,
+  close
+})
 </script>
 
-<template>12</template>
+<template>
+  <Transition
+    :name="transitionName"
+    @enter="() => (boxHeight = messageRef!.getBoundingClientRect().height)"
+    @after-leave="!visible && onDestory()"
+  >
+    <div
+      class="px-message"
+      ref="messageRef"
+      :class="{
+        [`px-message--${type}`]: type,
+        'is-close': showClose,
+        'text-center': center
+      }"
+      :style="customStyle"
+      v-show="visible"
+      role="alert"
+      @mouseenter="clearTimer"
+      @mouseleave="startTimer"
+    >
+      <px-icon class="px-message__icon" :icon="iconName" />
+      <div class="px-message__content">
+        <slot>
+          <render-vnode v-if="message" :vNode="message" />
+        </slot>
+      </div>
+      <div class="px-message__close" v-if="showClose">
+        <px-icon icon="times-solid" @click.stop="close" />
+      </div>
+    </div>
+  </Transition>
+</template>
 
 <style scoped>
 @import './style.css';
--- packages/components/Message/types.ts
+++ packages/components/Message/types.ts
@@ -1,6 +1,12 @@
 import type { Ref, VNode, ComponentInternalInstance } from 'vue'
 
-export const messageTypes = ['info', 'success', 'warning', 'danger'] as const
+export const messageTypes = [
+  'info',
+  'success',
+  'warning',
+  'danger',
+  'error'
+] as const
 
 export type MessageType = (typeof messageTypes)[number]
 
@@ -20,28 +26,81 @@ export interface Message extends MessageFn {
   warning: MessageTypeFn
   info: MessageTypeFn
   danger: MessageTypeFn
+  error: MessageTypeFn
 }
 
-export interface MessageProps {
+export interface MessagePropsIn {
   id: string
   message?: string | VNode | (() => VNode)
+  type?: MessageType
   duration?: number
   showClose?: boolean
   center?: boolean
-  type?: MessageType
   offset?: number
   zIndex: number
   transitionName?: string
   onDestory(): void
 }
+// api-table 文档展示用 props type
+export interface MessageProps {
+  /**
+   * @property message
+   * @type {string | VNode | (() => VNode)}
+   * @description 消息文字
+   * @default ''
+   */
+  message?: string | VNode | (() => VNode)
+  /**
+   * @property type
+   * @type enum - success | info | warning | danger | error
+   * @description 消息类型
+   * @default info
+   */
+  type?: MessageType
+  /**
+   * @property duration
+   * @type number
+   * @description 显示时间(ms), 设为0则不会自动关闭
+   * @default 3000
+   */
+  duration?: number
+  /**
+   * @property showClose
+   * @type boolean
+   * @description 是否显示关闭按钮
+   * @default false
+   */
+  showClose?: boolean
+  /**
+   * @property center
+   * @type boolean
+   * @description 是否居中显示
+   * @default false
+   */
+  center?: boolean
+  /**
+   * @property offset
+   * @type number
+   * @description 距离窗口顶部的位置偏移量
+   * @default 10
+   */
+  offset?: number
+  /**
+   * @property transitionName
+   * @type string
+   * @description 过渡动画名称
+   * @default fade-up
+   */
+  transitionName?: string
+}
 
-export type MessageOptions = Partial<Omit<MessageProps, 'id'>>
+export type MessageOptions = Partial<Omit<MessagePropsIn, 'id'>>
 export type MessageParams = string | VNode | MessageOptions
 
 export interface MessageInstance {
   id: string
   vnode: VNode
-  props: MessageProps
+  props: MessagePropsIn
   vm: ComponentInternalInstance
   handler: MessageHandler
 }
@@ -52,6 +111,6 @@ export interface MessageCompInstance {
 }
 
 export type CreateMessageProps = Omit<
-  MessageProps,
+  MessagePropsIn,
   'onDestory' | 'id' | 'zIndex'
 >
