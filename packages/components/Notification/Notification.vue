0535a72952aecbe3f3a3a934d39f2b5bfb1295f4
mmt817 <1669404371@qq.com>
2025-05-31 14:56:54 +0800

    feat: PxNotification组件结构搭建

---
+++ packages/components/Notification/Notification.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue'
+import { delay, bind } from 'lodash-es'
+import { useOffset } from '@pixel-ui/hooks'
+import { addUnit, typeIconMap, RenderVNode } from '@pixel-ui/utils'
+
+import type { NotificationPropsIn, NotificationCompInstance } from './types'
+
+import PxIcon from '../Icon/Icon.vue'
+
+const COMP_NAME = 'PxNotification' as const
+defineOptions({
+  name: COMP_NAME
+})
+
+const props = withDefaults(defineProps<NotificationPropsIn>(), {
+  type: 'info',
+  duration: 3000,
+  offset: 20,
+  position: 'top-right',
+  transitionName: 'fade',
+  showClose: true
+})
+
+const visible = ref(false)
+const notifyRef = ref<HTMLDivElement>()
+const iconName = computed(
+  () => props.icon ?? typeIconMap.get(props.type) ?? 'info-circle-solid'
+)
+// div 高度
+const boxHeight = ref(0)
+
+// const { topOffset, bottomOffset } = useOffset({
+//   getLastBottomOffset: bind(getLastBottomOffset, props),
+//   offset: props.offset,
+//   boxHeight
+// })
+
+// 水平位置
+const horizontalClass = computed(() =>
+  props.position.endsWith('right') ? 'right' : 'left'
+)
+// 垂直位置
+const verticalClass = computed(() =>
+  props.position.startsWith('top') ? 'top' : 'bottom'
+)
+
+// ⏲ notification 计时
+let timer: number
+function startTimmer() {
+  if (props.duration === 0) return
+  timer = delay(close, props.duration)
+}
+
+function clearTimer() {
+  clearTimeout(timer)
+}
+
+function close() {
+  visible.value = false
+}
+
+onMounted(() => {
+  visible.value = true
+  startTimmer()
+})
+
+// 暴露方法
+defineExpose<NotificationCompInstance>({
+  bottomOffset: ref(0),
+  close
+})
+</script>
+
+<template>
+  <transition
+    :name="`px-notification-${transitionName}`"
+    @after-leave="!visible && onDestory()"
+    @enter="boxHeight = notifyRef!.getBoundingClientRect().height"
+  >
+    <div
+      ref="notifyRef"
+      class="px-notification"
+      :class="{
+        [`px-notification--${type}`]: type,
+        [horizontalClass]: true,
+        'show-close': showClose
+      }"
+      :style="customStyle"
+      v-show="visible"
+      role="alert"
+      @click="onClick"
+      @mouseenter="clearTimer"
+      @mouseleave="startTimmer"
+    >
+      <px-icon v-if="iconName" :icon="iconName" class="px-notification__icon" />
+
+      <div class="px-notification__text">
+        <div class="px-notification__title">{{ title }}</div>
+        <div class="px-notification__content">
+          <slot>
+            <RenderVNode v-if="message" :vNode="message" />
+          </slot>
+        </div>
+      </div>
+      <div class="px-notification__close" v-if="showClose">
+        <px-icon icon="times-solid" @click.stop="close" />
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style scoped>
+@import './style.css';
+</style>
---
+++ packages/components/Notification/types.ts
@@ -0,0 +1,182 @@
+import type { VNode, Ref, ComponentInternalInstance } from 'vue'
+
+export const notificationTypes = [
+  'info',
+  'primary',
+  'success',
+  'warning',
+  'danger',
+  'sakura',
+  'error',
+  'iron'
+] as const
+
+export type NotificationType = (typeof notificationTypes)[number]
+
+export const notificationPosition = [
+  'top-right',
+  'top-left',
+  'bottom-right',
+  'bottom-left'
+] as const
+
+export type NotificationPosition = (typeof notificationPosition)[number]
+
+export interface NotificationHandler {
+  close(): void
+}
+
+export type NotificationFn = {
+  (_props: NotificationParams): NotificationHandler
+  closeAll(_type?: NotificationType): void
+}
+
+export type NotificationTypeFn = (
+  _props: NotificationParams
+) => NotificationHandler
+
+export interface Notification extends NotificationFn {
+  primary: NotificationTypeFn
+  sakura: NotificationTypeFn
+  success: NotificationTypeFn
+  warning: NotificationTypeFn
+  info: NotificationTypeFn
+  danger: NotificationTypeFn
+  error: NotificationTypeFn
+  iron: NotificationTypeFn
+}
+
+export interface NotificationPropsIn {
+  title: string
+  id: string
+  zIndex: number
+  position: NotificationPosition
+  type?: NotificationType
+  message?: string | VNode
+  duration?: number
+  showClose?: boolean
+  offset?: number
+  transitionName?: string
+  icon?: string
+  onClick?(): void
+  onClose?(): void
+  onDestory(): void
+}
+
+// api-table 文档展示用 props type
+export interface NotificationProps {
+  /**
+   * @property title
+   * @type string
+   * @description 通知标题
+   * @default -
+   */
+  title: string
+  /**
+   * @property message
+   * @type {string | VNode}
+   * @description 通知内容
+   * @default -
+   */
+  message?: string | VNode
+  /**
+   * @property type
+   * @type enum - primary | success | info | warning | danger | error | sakura | iron
+   * @description 通知类型
+   * @default info
+   */
+  type?: NotificationType
+  /**
+   * @property icon
+   * @type string
+   * @description 自定义图标
+   * @default -
+   */
+  icon?: string
+  /**
+   * @property duration
+   * @type number
+   * @description 显示时间(ms), 设为0则不会自动关闭
+   * @default 3000
+   */
+  duration?: number
+  /**
+   * @property showClose
+   * @type boolean
+   * @description 是否显示关闭按钮
+   * @default true
+   */
+  showClose?: boolean
+  /**
+   * @property offset
+   * @type number
+   * @description 距离窗口顶部的位置偏移量
+   * @default 20
+   */
+  offset?: number
+  /**
+   * @property transitionName
+   * @type string
+   * @description 过渡动画名称
+   * @default fade-up
+   */
+  transitionName?: string
+  /**
+   * @property position
+   * @type enum - top-right | top-left | bottom-right | bottom-left
+   * @description 通知位置
+   * @default top-right
+   */
+  position: NotificationPosition
+  /**
+   * @property onClose
+   * @type  Function - () => void
+   * @description 关闭回调
+   * @default -
+   */
+  onClose?(): void
+  /**
+   * @property onClick
+   * @type  Function - () => void
+   * @description 点击回调
+   * @default -
+   */
+  onClick?(): void
+}
+
+export type NotificationOptions = Partial<Omit<NotificationPropsIn, 'id'>>
+export type NotificationParams = string | VNode | NotificationOptions
+
+export interface NotificationInstance {
+  id: string
+  vnode: VNode
+  vm: ComponentInternalInstance
+  props: NotificationPropsIn
+  handler: NotificationHandler
+}
+
+export interface NotificationCompInstance {
+  close(): void
+  bottomOffset: Ref<number>
+}
+
+// api-table 文档展示用 comp-instance
+export interface NotificationExpose {
+  /**
+   * @property close
+   * @description 关闭消息
+   * @type  Function - () => void
+   */
+  close(): void
+  /**
+   * @property bottomOffset
+   * @description 消息底部距离视口顶偏移量
+   * @type  {Ref<number>}
+   */
+  bottomOffset: Ref<number>
+}
+
+export type CreateNotificationProps = Omit<
+  NotificationPropsIn,
+  'onDestory' | 'id' | 'zIndex'
+>
