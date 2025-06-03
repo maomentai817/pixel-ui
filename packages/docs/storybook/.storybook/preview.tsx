be313ccb1aa0d9acec29581db9cedebbe6b79342
mmt817 <1669404371@qq.com>
2025-06-03 20:15:40 +0800

    feat: storybook预加载脚本注册paintWorklets隔离环境

--- packages/docs/storybook/.storybook/preview.tsx
+++ packages/docs/storybook/.storybook/preview.tsx
@@ -2,6 +2,7 @@ import type { Preview } from '@storybook/vue3'
 
 import "@hackernoon/pixel-icon-library/fonts/iconfont.css"
 import '@mmt817/pixel-ui/dist/theme/index.css'
+import { registerPaintWorklets } from '@mmt817/pixel-ui'
 
 const preview: Preview = {
   parameters: {
@@ -13,5 +14,8 @@ const preview: Preview = {
     }
   }
 }
+
+// 预加载注册 paintWorklets
+registerPaintWorklets()
 
 export default preview
